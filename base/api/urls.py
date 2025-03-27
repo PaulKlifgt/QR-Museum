2e1ef5b12ad37223f3b6b2bcec851ae9023c7a43
PaulKlifgt <boevo.arty@yandex.ru>
2025-03-27 17:35:13 +1000

    created endpoints

--- base/api/urls.py
+++ base/api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from . import views
+
+
 urlpatterns = [
-    path(),
+    path('exh/<int:id>', views.get_exhibit_by_id, name='get_exhibit_by_id'),
+    path('sec/<int:id>', views.get_section_by_id, name='get_sectiont_by_id'),
+    path('exhs_by_sec/<int:section_id>', views.get_exhibit_by_section, name='get_exhibit_by_section'),
+    path('all_sec/', views.get_all_sections, name='get_all_sections')
 ]
