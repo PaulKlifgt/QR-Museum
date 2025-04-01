2bcc696b04557b2f61e8346dc42e03efa3ea0c9a
PaulKlifgt <boevo.arty@yandex.ru>
2025-04-01 11:05:35 +1000

    collectstatic

--- web-api/base/base/urls.py
+++ web-api/base/base/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import api
+import api.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('users/', include('users.urls')),
+    path('', api.views.index, name='index')
 ]
